47d011c2a80b42ad028c53c8342e8c159b61cce3
Grevisse Kenguruka <kengres@yandex.ru>
2022-01-25 13:16:11 +0200

    added 2 challenges

---
+++ say-hello.js
@@ -0,0 +1,11 @@
+// create a function that receives a name and log to the console Hello, name!
+// Pracy => "Hello, Pracy!"
+
+function getSum(name) {
+  // code here
+}
+
+getSum("Pracy");
+getSum("Grevisse");
+getSum("Collins");
+getSum("Muco");
---
+++ sum.js
@@ -0,0 +1,12 @@
+// create a function that receives 2 numbers and adds them
+// 4, 5 => 9
+// -4, 5 => 1
+
+function getSum(a, b) {
+  // code here
+}
+
+getSum("Pracy");
+getSum("Grevisse");
+getSum("Collins");
+getSum("Muco");
