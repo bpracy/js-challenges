cf156a098d2ab6b7ded4cc0c70ea665e5b54deda
bpracy <pracyline@gmail.com>
2022-02-12 21:07:59 +0300

    exo fini

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
 // todo: create a todo list with js
-const todos = [];
+const todosList = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "illo expedita consequatur quia in",
+    "completed": false
+  },
+  ];
 
 const getTodoHtml = (todo) => {
+  const date = new Date().toDateString();
   return `
     <div class="todo">
       <div class="todo-title">${todo.title}</div>
-    </div>
-  `
+      <div class="todo-date">${date}</div>
+      <br>
+    </div>`
 }
 
 // construire le html de tous les todos
-// inserer le html fianl dans le #todos
+let todosHtml = "";
+for (const todo of todosList){
+  todosHtml += getTodoHtml(todo);
+}
+// inserer le html final dans le #todos
+const todos = document.querySelector('.todos');
+todos.innerHTML = todosHtml;
 // css
